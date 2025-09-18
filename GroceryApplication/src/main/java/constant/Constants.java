f8d956eac8b9e47caa00f99f9f9322e1b5a1ac38
Anu <Anu@DESKTOP-952Q9QQ>
2025-09-18 08:13:08 +0530

    First Project

--- GroceryApplication/src/main/java/constant/Constants.java
+++ GroceryApplication/src/main/java/constant/Constants.java
@@ -1,8 +1,9 @@
 package constant;
 
 public class Constants {
-	public static final String TESTDATAFILE=System.getProperty("user.dir")+"\\src\\test\\resources\\TestData.xlsx";
+	public static final String TESTDATAFILE=System.getProperty("user.dir")+"\\src\\test\\resources\\TestData .xlsx";
     public static final String LOGINSHEET="LoginPage";
     public static final String ADMINSHEET="HomePage";
     public static final String CONFIGFILE=System.getProperty("user.dir")+"\\src\\main\\resources\\config.properties";
 }
+
